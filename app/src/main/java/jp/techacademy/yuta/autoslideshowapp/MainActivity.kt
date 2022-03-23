4d34fc44094d0b92581d12d0862c42046c54f325
yu2ta <yuta_matsuo@r.recruit.co.jp>
2022-03-23 18:51:11 +0900

    課題提出

---
+++ app/src/main/java/jp/techacademy/yuta/autoslideshowapp/MainActivity.kt
@@ -0,0 +1,131 @@
+package jp.techacademy.yuta.autoslideshowapp
+
+import android.Manifest
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentUris
+import android.database.Cursor
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.timer
+
+class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+    private var mTimer: Timer? = null
+    private var mHandler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo()
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+        }
+    }
+
+    private fun getContentsInfo() {
+        // 画像の情報を取得する
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目（null = 全項目）
+            null, // フィルタ条件（null = フィルタなし）
+            null, // フィルタ用パラメータ
+            null // ソート (nullソートなし）
+        )
+
+        cursor!!.moveToFirst()
+
+        // 初回表示
+        val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = cursor.getLong(fieldIndex)
+        val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        imageView.setImageURI(imageUri)
+
+        //次へボタンが押されたとき
+        bt_next.setOnClickListener{
+            goNext(cursor)
+        }
+
+        //前へボタンが押された時
+        bt_before.setOnClickListener{
+            goBefore(cursor)
+        }
+
+        //再生ボタンが押された時
+        bt_start.setOnClickListener{
+            if(bt_start.text.equals("再生")) {
+                bt_start.text = "停止"
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask() {
+                    override fun run() {
+                        mHandler.post {
+                            goNext(cursor)
+                        }
+                    }
+                }, 2000, 2000)
+            } else {
+                if (mTimer != null){
+                    bt_start.text = "再生"
+                    mTimer!!.cancel()
+                    mTimer = null
+                }
+            }
+        }
+    }
+
+    private fun goNext(cursor: Cursor) {
+        Log.d("garally", "next")
+        if (cursor.isLast) {
+            cursor.moveToFirst()
+        } else {
+            cursor.moveToNext()
+        }
+
+        val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = cursor.getLong(fieldIndex)
+        val imageUri =ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        imageView.setImageURI(imageUri)
+    }
+
+    private fun goBefore(cursor: Cursor) {
+        Log.d("garally", "before")
+        if (cursor.isFirst) {
+            cursor.moveToLast()
+        } else {
+            cursor.moveToPrevious()
+        }
+
+        val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = cursor.getLong(fieldIndex)
+        val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        imageView.setImageURI(imageUri)
+    }
+}
